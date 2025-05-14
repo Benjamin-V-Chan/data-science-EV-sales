46f528bee307f9cdb202ce449760c39123ab61ad
BenjaminChan <benjamin.v.chan@gmail.com>
2025-05-13 23:51:20 -0700

    07 script outline

---
+++ scripts/07_visualization_reports.py
@@ -0,0 +1,6 @@
+# Load customer_segments.csv from outputs
+# Plot and save:
+#   • Trend of total Units_Sold by Year (outputs/yearly_sales_trend.png)
+#   • Scatter of Battery_Capacity_kWh vs. Units_Sold (outputs/battery_vs_sales.png)
+#   • Bar chart of cluster counts (outputs/cluster_distribution.png)
+# Print “Visualizations saved” once done
